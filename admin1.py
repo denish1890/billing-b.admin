bbbd9cb237907d71f1e74709791dd4390deb963e
denish1890 <rakholiyadenish@gmail.com>
2026-03-11 22:25:48 +0530

    Update admin1.py

--- admin1.py
+++ admin1.py
@@ -1005,11 +1005,12 @@ if st.session_state["page"] == "Admin":
                     # Proceed with saving if validation passes
                     image_path = None
                     if image:
-                        os.makedirs("menu_images", exist_ok=True)
-                        image_path = os.path.join("menu_images", image.name)
-                        with open(image_path, "wb") as f:
-                            f.write(image.getbuffer())
-            
+                        filename = f"{uuid.uuid4()}_{image.name}"
+                        image_path = os.path.join("menu_images", filename)
+                        full_path = os.path.join(IMAGE_DIR, filename)
+
+                    with open(full_path, "wb") as f:
+                        f.write(image.getbuffer())
                     variants_json = json.dumps(variant_data) if variant_data else None
                     base_price = min([v["price"] for v in variant_data]) if variant_data else 0
             
@@ -2113,6 +2114,7 @@ if st.session_state["page"] == "downloadbill":
 
 
     
+
 
 
 
